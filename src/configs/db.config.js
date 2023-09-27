d46fe7dcb3948d7a086f84998db77a2e07189c95
mbel mohamed <medfac2010@gmail.com>
2023-09-27 06:04:06 +0100

    Uptodate the repository

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,15 +1,15 @@
 const env = process.env;
-const fs = require('fs');
+//const fs = require('fs');
 const db = {
     host: env.DB_HOST,
     user: env.DB_USER,
     password: env.DB_PASSWORD,
-    database: env.DB_NAME || 'programming_languages',
+    database: env.DB_NAME,
     port: env.DB_PORT || 3306,
-    ssl: {
+  /*   ssl: {
       mode: 'VERIFY_IDENTITY',
       ca: fs.readFileSync('/etc/ssl/cert.pem', 'utf-8'),
     }
-};
+ */};
 
 module.exports = db;
